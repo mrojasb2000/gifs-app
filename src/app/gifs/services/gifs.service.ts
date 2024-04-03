22d210f5856196ba3164f65cc1f464d82dd6d1e3
Mauricio Rojas <mrojasb2000@gmail.com>
2024-04-03 12:47:13 -0300

    feat: adding local storage search tags

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
   private _tagsHistory: string[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+   }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -27,6 +29,20 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this._tagsHistory.splice(0,20);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+     this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+     if (this._tagsHistory.length === 0) return;
+     this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
